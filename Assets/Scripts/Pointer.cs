515d368c08bf559f30a87e7a5fb5515607d909f7
Carlos Sánchez Abad <98030188+thecharlie00@users.noreply.github.com>
2022-04-04 14:57:40 +0200

    Catch hecho

--- Assets/Scripts/Pointer.cs
+++ Assets/Scripts/Pointer.cs
@@ -13,10 +13,24 @@ public class Pointer : MonoBehaviour
     void Update()
     {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Catch();
     }
 
     private void FixedUpdate()
     {
         pointer.transform.position = mousePos;
+    }
+
+    private void Catch()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, 100f);
+            if(hit.collider)
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
     }
 }
